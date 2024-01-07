10fcb261640650c10c8e7320e1106fb0f7b911a3
Przemo <przemo06133@gmail.com>
2024-01-07 21:23:47 +0100

    Install Openzeppelin contracts upgradeable

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,10 +1,13 @@
-// SPDX-License-Idendifier: MIT
+// SPDX-License-Identifier: MIT
 
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-contract BoxV1 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
+
+    function setNumber(uint256 _number) external {}
 
     function getNumber() external view returns (uint256) {
         return number;
@@ -13,4 +16,6 @@ contract BoxV1 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
