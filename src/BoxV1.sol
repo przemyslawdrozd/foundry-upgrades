f2426f40e0688b54798a5cf51ebfd8cadd084eb8
Przemo <przemo06133@gmail.com>
2024-01-14 21:27:39 +0100

    Add initialize func to create proxy for contract

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -6,12 +6,17 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
     }
 
     function setNumber(uint256 _number) external {}
